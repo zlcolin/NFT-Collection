374847f51b378fab6de3d113c9606738a91c1c8d
zlcolin <zlcolin@gmail.com>
2022-07-11 20:46:48 +0800

    modified by c

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x8cF84D502783225B825CDAe6d2de6cbBd3Ee9d57";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-swart-rho.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
